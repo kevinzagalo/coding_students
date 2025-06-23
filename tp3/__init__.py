0fd15a2d98e98834eac9da8a08704d7e80747ce6
Kevin Zagalo <kevinzagalo@me.com>
2025-06-23 15:23:21 +0200

    v2
    deso Lélio mais on va switcher sur un truc plus simple +1
    
    Co-Authored-By: LelioC <17880128+lelioc@users.noreply.github.com>

--- tp3/__init__.py
+++
@@ -1,5 +0,0 @@
-from .huffman_node import HuffmanNode
-from .interactive_grid import InteractiveGrid
-from .tables import letter_frequencies
-from .qrcodes import qrcode
-
--- tp3/qrcodes.py
+++ tp3/qrcodes.py
@@ -1,7 +1,8 @@
+import pandas as pd
+
 import numpy as np
 import pandas as pd
 import galois
-from .tables import *
 
 
 class qrcode:
@@ -506,3 +507,4 @@ class qrcode:
         
         modules = (modules > 0).astype(int)
         return modules
+
